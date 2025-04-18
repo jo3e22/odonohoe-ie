dfc62ab4b86d82c31401cdd49ad42a37759fca09
James O'Donohoe <148874537+jo3e22@users.noreply.github.com>
2025-04-18 11:34:20 +0000

    Remove mobile menu links for CV, About, and Contact to streamline navigation

--- header-mobile.php
+++ header-mobile.php
@@ -3,12 +3,5 @@
         <div class="logo">
             <a href="index.php"><img src="images/logo-122620-fffef9.png" alt="My Website Logo" style="height:80px;"></a>
         </div>
-        <div class="mobile-menu">
-            <ul>
-                <li><a href="#cv"><?php echo $lang['cv-short']; ?></a></li>
-                <li><a href="#about"><?php echo $lang['about']; ?></a></li>
-                <li><a href="#contact"><?php echo $lang['contact']; ?></a></li>
-            </ul>
-        </div>
     </nav>
 </header>
